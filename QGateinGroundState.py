dd12b7276be7056dc0e36d3eeffaa561b04965cc
Charles Barnes <onthisrock97@gmail.com>
2022-04-05 13:16:45 -0400

    Introduction to Quantum Cmputing utilizing a ariety of different circuits

---
+++ QGateinGroundState.py
@@ -0,0 +1,11 @@
+#First Quantum Gate (In Ground State)
+from qiskit import QuantumRegister, ClassicalRegister, QuantumCircuit
+from numpy import pi
+
+qreg_q = QuantumRegister(3, 'q')
+creg_c = ClassicalRegister(3, 'c')
+circuit = QuantumCircuit(qreg_q, creg_c)
+
+circuit.reset(qreg_q[0])
+circuit.x(qreg_q[0])
+circuit.measure(qreg_q[0], creg_c[0])
